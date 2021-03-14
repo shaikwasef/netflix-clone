442c558ff885f11b419176bfb39afec00eb7e961
wasefshaik@gmail.com <wasefshaik@gmail.com>
2021-03-14 14:26:36 +0530

    homescreen and navbar intialized

---
+++ src/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./HomeScreen.css"
+
+function HomeScreen() {
+    return (
+        <div className = "homeScreen">
+            
+
+        </div>
+    )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRNFfbkSNgwbxesO8XjV-cePpG5Ge-zBs",
+    authDomain: "netflix-clone-c750b.firebaseapp.com",
+    projectId: "netflix-clone-c750b",
+    storageBucket: "netflix-clone-c750b.appspot.com",
+    messagingSenderId: "720681416702",
+    appId: "1:720681416702:web:8a85c9b435d7a2175c72af"
+  };
