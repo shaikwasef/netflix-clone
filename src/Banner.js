934d6a43298c8793b94e9f6bd1396f808b2067af
shaik wasef <wasefshaik@gmail.com>
2021-04-27 11:17:43 +0530

    comments and arrows added

--- src/Banner.js
+++ src/Banner.js
@@ -1,26 +1,30 @@
+//comments
 import React, { useEffect , useState } from 'react'
 import "./Banner.css"
 import axios from "./axios"
 import requests from "./Requests"
 
 function Banner() {
+    //set top banner 
     const [movie,setMovie] = useState([]);
 
+    //useEffect to fetch the latest netflix orginals on each component mount
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(requests.fetchNetflixOriginals);
+            //Set a random poster on every mount of the component using random function
             setMovie(
                 request.data.results[Math.floor(Math.random()*request.data.results.length-1)]
             );
             return request;
         }
+        // call the fetch data function
         fetchData();
     },[]);
     
-    console.log(movie);
-
+    // Truncating the description
     function truncate(string , n){
-        return string?.length > n ? string.substr(0,n-1) + '...' : string ;
+        return string.length > n ? string.substr(0,n-1) + '...' : string ;
     }
 
     return (
@@ -32,9 +36,9 @@ function Banner() {
 
         <div className = "banner_contents">
             <h1 className = "banner_title">{movie?.original_title}</h1>
-            <div className = "banner_buttons">
-                <button className = "banner_button">Play</button>
-                <button className = "banner_button">My List</button>
+                 <div className = "banner_buttons">
+                    <button className = "banner_button">Play</button>
+                    <button className = "banner_button">My List</button>
                 </div>
                 <h1 className = "banner_description" > {truncate(`${movie?.overview}`,200)} </h1>
             </div> 
